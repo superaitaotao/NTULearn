f19cad7c06aba8e0f9858e11b78afb8866744e4f
superaitaotao <superaitaotao@gmail.com>
2016-11-08 19:09:00 +0800

    Initial Commit

---
+++ NewNTULearn/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NewNTULearn
+//
+//  Created by shutao xu on 8/11/16.
+//  Copyright © 2016 shutao xu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
